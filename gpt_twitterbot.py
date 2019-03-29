00a034b5d388040b47096a32a2ce9646a0bb73b4
Philip Mohun <AG01310@LC02WL1KEHTDD>
2019-03-29 15:08:18 -0700

    updated bot to test tweepy API and Heroku hosting

--- gpt_twitterbot.py
+++ gpt_twitterbot.py
@@ -4,26 +4,27 @@
 import tweepy, time, sys
 from os import environ
 
-argfile = str(sys.argv[1])
+##argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+ACCESS_SECRET = environ['ACCESS_SECRET']'
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
+#filename=open(argfile,'r')
+#f=filename.readlines()
+#filename.close()
 sleep = 60 * 60; # 1 hour interval
 i = 1
 while i > 0:
     new_line = i
-    api .update_status(str(new_line)
-    i + 1
+    api .update_status(str(new_line))
+    i += 1
     time.sleep(sleep)
 
 '''for line in f:
