2e6d1a13ca3b60e5f9892062d80a3555f23d1d58
Philip Mohun <pmohun@users.noreply.github.com>
2019-04-03 11:20:24 -0700

    Removed trailing ' that was causing SyntaxError

--- gpt_twitterbot.py
+++ gpt_twitterbot.py
@@ -10,7 +10,7 @@ from os import environ
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']'
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
